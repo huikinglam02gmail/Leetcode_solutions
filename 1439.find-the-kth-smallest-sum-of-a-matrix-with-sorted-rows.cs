54986fd14b4f1e59b0c2eec840feec664f0378e8
huikinglam02gmail <huikinglam02@gmail.com>
2022-11-06 05:10:34 -0500

    110622

---
+++ 1439.find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.cs
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1439 lang=csharp
+ *
+ * [1439] Find the Kth Smallest Sum of a Matrix With Sorted Rows
+ */
+
+// @lc code=start
+public class Solution {
+    public int KthSmallest(int[][] mat, int k) {
+        int prev = Int32.MinValue;
+        return 0;
+    }
+}
+// @lc code=end
+
