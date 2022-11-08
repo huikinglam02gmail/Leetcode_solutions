6899ee6da52dedeac2ad6fee43ec3a2637cfb0b9
huikinglam02gmail <huikinglam02@gmail.com>
2022-11-07 21:02:10 -0500

    1544_110722

--- 1442.count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442.count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -7,31 +7,12 @@
 # @lc code=start
 from typing import List
 class Solution:
-    # Make use of XOR property
-    # Prepare prefix XOR for the array
-    # Requirement: i < j, but j <= k
-    # a = prefix[j] ^ prefix[i]
-    # b = prefix[k+1]^prefix[j]
-    # We are looking for prefix[i]^prefix[j] = prefix[k+1]^prefix[j]
-    # We can prepare pairwise XOR between all prefix XORs
+    # The problem is asking for contiguous nonempty subarrays which have equal XOR
+    # But we XOR them together, we will always arrive at 0
+    # So the problem is to find all subarrays with 0 XOR
+    # Each with contribute len(0 XOR subarray) - 1 to the result
 
     def countTriplets(self, arr: List[int]) -> int:
-        prefix, n, result = [0], len(arr), 0
-
-        for i, num in enumerate(arr):
-            prefix.append(prefix[-1]^num)
         
-        hashTableEnd = {}
-        for i in range(n):
-            for j in range(i+1,n+1):
-                pair = prefix[i]^prefix[j]
-                if pair not in hashTableEnd:
-                    hashTableEnd[pair] = {}
-                if j not in hashTableEnd[pair]:
-                    hashTableEnd[pair][j] = 0
-                hashTableEnd[pair][j] += 1
-                if i in hashTableEnd[pair]:
-                    result += hashTableEnd[pair][i]
-        return result
 # @lc code=end
 
