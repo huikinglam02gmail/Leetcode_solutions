6c28cd4be327635a2ed8e1d7f5e8d4bfc01cd742
huikinglam02gmail <huikinglam02@gmail.com>
2022-12-18 15:57:38 -0500

    1547_121822

---
+++ 1547.minimum-cost-to-cut-a-stick.cs
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=1547 lang=csharp
+ *
+ * [1547] Minimum Cost to Cut a Stick
+ */
+
+// @lc code=start
+public class Solution 
+{
+    int[] Cuts;
+    Dictionary<Tuple<int, int>, int> memo = new Dictionary<Tuple<int, int>, int>();
+
+    public int MinCost(int n, int[] cuts) 
+    {
+        Cuts = new int[2 + cuts.Length];
+        Cuts[0] = 0;
+        Cuts[Cuts.Length - 1] = n;
+        Array.Sort(cuts);
+        for (int i = 0; i < cuts.Length; i++)
+        {
+            Cuts[i + 1] = cuts[i];
+        }
+        return dp(0, Cuts.Length - 1);
+    }
+
+    public int dp(int i, int j)
+    {
+        if (i == j - 1)
+        {
+            return 0;
+        }
+        Tuple<int, int> t = new Tuple<int, int> (i, j);
+        if (memo.ContainsKey(t))
+        {
+            return memo[t];
+        }
+        else
+        {
+            int cost = Int32.MaxValue;
+            for (int k = i + 1; k < j; k++)
+            {
+                cost = Math.Min(cost, dp(i, k) + dp(k, j));
+            }
+            int result = Cuts[j] - Cuts[i] + cost;
+            memo.Add(t, result);
+            return result;
+        }
+    }
+}
+// @lc code=end
+
---
+++ 1547.minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=1547 lang=python3
+#
+# [1547] Minimum Cost to Cut a Stick
+#
+
+# @lc code=start
+from functools import lru_cache
+from typing import List
+
+
+class Solution:
+    # A classic DP problem
+    # Add 0 and n to cuts
+    # Sort cuts to faciliate labelling and extraction of length
+    # So let dp[i][j] = minimum total cost to cut the stick between i and j
+    # We are looking for dp[0][l], l = len(cut)
+    # As long as i != j - 1, we can look for the minimum of cuts[j] - cuts[i] + min(dp[i][k] + dp[k][j]), i < k < j
+
+    @lru_cache(None)
+    def dp(self, i, j):
+        if i == j - 1:
+            return 0
+        result = self.cuts[j] - self.cuts[i]
+        extra = float('Inf')
+        for k in range(i + 1, j):
+            extra = min(extra, self.dp(i, k) + self.dp(k, j))
+        return result + extra 
+    
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        self.cuts = [0] + sorted(cuts) + [n]
+        return self.dp(0, len(self.cuts)-1)
+# @lc code=end
+
