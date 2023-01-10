3f98376c8b8d0179a4c638b5b3cbb8a65330a3e7
huikinglam02gmail <huikinglam02@gmail.com>
2023-01-09 21:21:34 -0500

    commit

---
+++ 100.same-tree.cs
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=100 lang=csharp
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public bool IsSameTree(TreeNode p, TreeNode q) 
+    {    
+        if (p == null)
+        {
+            return q == null;
+        }
+        else if (q == null)
+        {
+            return p == null;
+        }
+        else if (p.val != q.val)
+        {
+            return false;
+        }
+        else
+        {
+            return (IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
+        }
+    }
+}
+// @lc code=end
+
