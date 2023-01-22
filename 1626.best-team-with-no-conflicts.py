e07df36e72812992d123c548afaa81d9818cd7e9
huikinglam02gmail <huikinglam02@gmail.com>
2023-01-22 05:41:47 -0500

    131_012223

---
+++ 1626.best-team-with-no-conflicts.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1626 lang=python3
+#
+# [1626] Best Team With No Conflicts
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        
+# @lc code=end
+
