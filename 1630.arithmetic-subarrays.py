670b28eba8980379f86debfde3d581dc9b33334f
huikinglam02gmail <huikinglam02@gmail.com>
2023-01-26 02:48:52 -0500

    909_1627_2359_012623

---
+++ 1630.arithmetic-subarrays.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=1630 lang=python3
+#
+# [1630] Arithmetic Subarrays
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    # No shortcut, just brute force
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        
+# @lc code=end
+
---
+++ 2359.find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=2359 lang=python3
+#
+# [2359] Find Closest Node to Given Two Nodes
+#
+
+# @lc code=start
+from collections import deque
+from typing import List
+
+
+class Solution:
+    # BFS fromn node1 to all other nodes, record the distance
+    # Do the same for node2
+    # Then find the index
+    def bfs(self, startNode, distanceArray):
+        dq = deque()
+        visited = set()
+        dq.append(startNode)
+        visited.add(startNode)
+        steps = 0
+        while dq:
+            for i in range(len(dq)):
+                node = dq.popleft()
+                distanceArray[node] = min(distanceArray[node], steps)
+                for nxt in self.graph[node]:
+                    if nxt not in visited:
+                        visited.add(nxt)
+                        dq.append(nxt)
+            steps += 1
+        return distanceArray
+    
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        n = len(edges)
+        dist1 = [n]*n
+        dist2 = [n]*n
+        self.graph = [set() for i in range(n)]
+        for i, nxt in enumerate(edges):
+            if nxt != -1:
+                self.graph[i].add(nxt)
+
+        dist1 = self.bfs(node1, dist1)
+        dist2 = self.bfs(node2, dist2)
+        
+        minSoFar = n
+        minIndex = -1
+        for i in range(n):
+            if max(dist1[i], dist2[i]) < minSoFar:
+                minIndex = i
+                minSoFar = max(dist1[i], dist2[i])
+        return minIndex
+        
+
+# @lc code=end
+
