1bce898d7e919e0777c18c3120fc67d49c81454d
huikinglam02gmail <huikinglam02@gmail.com>
2023-02-17 04:27:57 -0500

    783_021723

---
+++ 1655.distribute-repeating-integers.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1655 lang=python3
+#
+# [1655] Distribute Repeating Integers
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def canDistribute(self, nums: List[int], quantity: List[int]) -> bool:
+        
+# @lc code=end
+
