ce13b47deec4fc103fc91e7c66bc08551209f9c0
huikinglam02gmail <huikinglam02@gmail.com>
2023-02-17 23:17:58 -0500

    226_021723

--- 1655.distribute-repeating-integers.py
+++ 1655.distribute-repeating-integers.py
@@ -5,11 +5,27 @@
 #
 
 # @lc code=start
+from collections import Counter
 from typing import List
 
 
 class Solution:
+    # There are only 10 customers and 50 types of nums
+    # So we can use bitmask to use bitmask[ith bit] = 1 to represent customer[i] is satisfied
+    # Then we can save all the possible configurations in which a certain bitmask is satisfied
+    # Then we ask for if dp[2^m - 1] is true
+    # To look for a possible solution, we backtrack: for each customer
+    # we ration the appropriate amount to any ok customer, and further dfs into the  
     def canDistribute(self, nums: List[int], quantity: List[int]) -> bool:
-        
+        self.hashTable =  Counter(nums)
+        m = len(quantity)
+        dp = [False for i in range(1 << m)]
+        dp[0] = {}
+        okSet = [[] for i in range(1 << m)]
+        for i in range(1 << m):
+            binI = bin(i)[2:]
+            for j in range(len(binI)):
+                if i & (1 << j) != 0:
+                    
 # @lc code=end
 
