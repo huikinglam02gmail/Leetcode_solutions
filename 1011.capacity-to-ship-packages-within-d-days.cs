d46a644c1eb5a1c3decca9988fc63ce5037b6ab3
huikinglam02gmail <huikinglam02@gmail.com>
2023-02-24 01:51:48 -0500

    632_1011_022423

---
+++ 1011.capacity-to-ship-packages-within-d-days.cs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1011 lang=csharp
+ *
+ * [1011] Capacity To Ship Packages Within D Days
+ */
+
+// @lc code=start
+public class Solution 
+{
+    public int ShipWithinDays(int[] weights, int days) 
+    {
+        
+    }
+}
+// @lc code=end
+
