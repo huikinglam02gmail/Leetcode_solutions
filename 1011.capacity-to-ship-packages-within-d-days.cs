8b1809d7c21277c7a3f72029d3ff148ca23c7b47
huikinglam02gmail <huikinglam02@gmail.com>
2023-02-25 23:56:27 -0500

    1011_022523

--- 1011.capacity-to-ship-packages-within-d-days.cs
+++ 1011.capacity-to-ship-packages-within-d-days.cs
@@ -5,11 +5,59 @@
  */
 
 // @lc code=start
+using System.Collections.Generic;
+using System;
+using System.Linq;
 public class Solution 
 {
+    List<int> prefix = new List<int>() {0};
+    
+    private int getdays(int capacity)
+    {
+        int day = 0;
+        int i = 0;
+        int j = 0;
+        while (i < prefix.Count)
+        {
+            while (j < prefix.Count && prefix[j] - prefix[i] <= capacity)
+            {
+                j++;
+            }
+            if (j < prefix.Count && prefix[j] - prefix[i] > capacity)
+            {
+                i = j - 1;
+            }
+            else
+            {
+                i = j;
+            }
+            day++;
+       }
+       return day;
+    }
+
     public int ShipWithinDays(int[] weights, int days) 
     {
-        
+        foreach (int weight in weights)
+        {
+            prefix.Add(prefix[prefix.Count - 1] + weight);
+        }
+
+        int left = weights.Max();
+        int right = prefix[prefix.Count - 1];
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (getdays(mid) <= days)
+            {
+                right = mid;
+            }            
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
 // @lc code=end
