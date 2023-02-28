0dad5077495c3beb981bc845cbc117862e6cb03d
huikinglam02gmail <huikinglam02@gmail.com>
2023-02-28 02:01:23 -0500

    652_022823

---
+++ 72.edit-distance.cs
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=72 lang=csharp
+ *
+ * [72] Edit Distance
+ */
+
+// @lc code=start
+public class Solution 
+{
+    public int MinDistance(string word1, string word2) {
+        
+    }
+}
+// @lc code=end
+
---
+++ 72.edit-distance.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=72 lang=python3
+#
+# [72] Edit Distance
+#
+
+# @lc code=start
+class Solution:
+    '''
+    dp[i][j] = minimum number of operations required to convert word1[:i] and word2[:j]
+    Include empty strings
+    dp operates on the end of word1[i] and word2[j]
+    Insert dp[i-1][j]
+    Delete dp[i][j-1]
+    Replace dp[i-1][j-1]    
+    '''
+   
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[0 for i in range(len(word2)+1)] for j in range(len(word1)+1)]
+        for i in range(len(word1)+1):
+            dp[i][0] = i
+        for i in range(len(word2)+1):
+            dp[0][i] = i
+        for i in range(1,len(word1)+1):
+            for j in range(1,len(word2)+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+        return dp[len(word1)][len(word2)]
+# @lc code=end
+
