9f822b9d1fb2aff85f36b2b272aaad8d0bc2b172
huikinglam02gmail <huikinglam02@gmail.com>
2023-03-04 00:33:34 -0500

    1667_030423

---
+++ 1667.fix-names-in-a-table.sql
@@ -0,0 +1,12 @@
+--
+-- @lc app=leetcode id=1667 lang=mysql
+--
+-- [1667] Fix Names in a Table
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT user_id, CONCAT(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2,LENGTH(name)))) AS "name"
+FROM Users ORDER BY user_id
+-- @lc code=end
+
