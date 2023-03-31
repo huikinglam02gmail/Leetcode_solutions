d69a2b2fcbc020b83cd42374d95c5203540e2a3e
huikinglam02gmail <huikinglam02@gmail.com>
2023-03-31 01:09:33 -0400

    1444_033123

--- 1444.number-of-ways-of-cutting-a-pizza.py
+++ 1444.number-of-ways-of-cutting-a-pizza.py
@@ -6,32 +6,33 @@
 
 # @lc code=start
 from functools import lru_cache
+from typing import List
 
 class Solution:
-    # DP problem
-    # dp(i,j,c) = Return the number of ways of cutting pizza[i:][j:]
-    # such that each piece contains at least one apple and there are c cuts left
-    # In order to make sure the pieces after cut has at least one apple
-    # We reuse code from 304. Range Sum Query 2D - Immutable that uses prefix sum
+    '''
+    DP problem. dp(i,j,c) = number of ways of cutting pizza[i:][j:], such that each piece contains at least one apple and there are c cuts left
+    In order to make sure the pieces after cut has at least one apple
+    We reuse code from 304. Range Sum Query 2D - Immutable that uses prefix sum
+    '''
 
     @lru_cache(None)
     def dp(self, i, j, c):
         if c == 0:
-            if self.sumRegion(i, j, self.m-1, self.n-1) >= 1:
+            if self.sumRegion(i, j, self.m-1, self.n-1) > 0:
                 return 1
             else:
                 return 0
-        if self.sumRegion(i, j, self.m-1, self.n-1) == 0:
-            return 0
-        else:
-            result = 0
+        result = 0
+        if self.sumRegion(i, j, self.m-1, self.n-1) > 0:
             for l in range(1, self.m + 1):
                 if self.sumRegion(i, j, l-1, self.n-1) >= 1:
                     result += self.dp(l, j, c - 1)
+                    result %= self.MOD
             for l in range(1, self.n + 1):
                 if self.sumRegion(i, j, self.m-1, l-1) >= 1:
                     result += self.dp(i, l, c - 1)
-            return result                
+                    result %= self.MOD
+        return result                
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.P[row2+1][col2+1] - self.P[row1][col2+1] - self.P[row2+1][col1] + self.P[row1][col1]
@@ -44,7 +45,7 @@ class Solution:
                 self.P[i][j] = self.P[i-1][j] + self.P[i][j-1] - self.P[i-1][j-1]
                 if pizza[i-1][j-1] == 'A':
                     self.P[i][j] += 1
-        return self.dp(0,0,k-1) % self.MOD
+        return self.dp(0,0,k-1)
     
         
 # @lc code=end
--- 1703.minimum-adjacent-swaps-for-k-consecutive-ones.py
+++ 1703.minimum-adjacent-swaps-for-k-consecutive-ones.py
@@ -5,7 +5,14 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
+    '''
+    Remember one key point: the median of a sequence is where we should move all sequence elements to if we want to make all elements equal (Leetcode 462).
+    So we record occurence of 1 in nums. Then we consider k subarrays and decide how many moves to create a consecutive k 1s around the median
+    '''
     def minMoves(self, nums: List[int], k: int) -> int:
         
 # @lc code=end
