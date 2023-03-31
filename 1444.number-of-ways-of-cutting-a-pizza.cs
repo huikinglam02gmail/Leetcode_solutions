c7c3f0134d0b6b8eeddbe1ac53b9fef2e7e8c8c0
huikinglam02gmail <huikinglam02@gmail.com>
2023-03-31 01:09:37 -0400

    commit

--- 1444.number-of-ways-of-cutting-a-pizza.cs
+++ 1444.number-of-ways-of-cutting-a-pizza.cs
@@ -5,19 +5,22 @@
  */
 
 // @lc code=start
+using System.Collections.Generic;
+using System;
+using System.Linq;
 public class Solution 
 {
     int[][] P;
     int m;
     int n;
-    long MOD = 100000007;
+    long MOD = 1000000007;
     Dictionary<Tuple<int,int,int>, long> memo = new Dictionary<Tuple<int,int,int>, long>();
-    int sumRegion(int row1, int col1, int row2, int col2)
+    public int sumRegion(int row1, int col1, int row2, int col2)
     {
         return P[row2+1][col2+1] + P[row1][col1] - P[row1][col2+1] - P[row2+1][col1];
     }
 
-    long dp(int i, int j, int c)
+    public long dp(int i, int j, int c)
     {
         Tuple<int, int, int> key = new Tuple<int, int, int>(i, j, c);
         if (c == 0)
@@ -31,18 +34,10 @@ public class Solution
                 return 0;
             }
         }       
-        if (memo.ContainsKey(key))
+        if (!memo.ContainsKey(key))
         {
-            return memo[key];
-        }
-        else
-        {
-            long result;
-            if (sumRegion(i, j, m-1, n-1) == 0)
-            {
-                return 0;
-            }
-            else
+            long result = 0;
+            if (sumRegion(i, j, m-1, n-1) > 0)
             {
                 result = 0;
                 for (int l = 1; l < m + 1; l++)
@@ -50,6 +45,7 @@ public class Solution
                     if (sumRegion(i, j, l-1, n-1) >= 1)
                     {
                         result += dp(l, j, c - 1);
+                        result %= MOD;
                     }
                 }
                 for (int l = 1; l < n + 1; l++)
@@ -57,24 +53,20 @@ public class Solution
                     if (sumRegion(i, j, m-1, l-1) >= 1)
                     {
                         result += dp(i, l, c - 1);
+                        result %= MOD;
                     }
                 }
             }
             memo.Add(key, result);
-            return result;
         }
+        return memo[key];
     }
     public int Ways(string[] pizza, int k) 
     {
         m = pizza.Length;
         n = pizza[0].Length;
-        P = new int[m+1][];
-        for (int i = 0; i < m + 1; i++)
-        {
-            P[i] = new int[n+1];
-            Array.Fill(P[i], 0);
-        }
-        for (int i = 1; i< m + 1; i++)
+        P = Enumerable.Range(0, m + 1).Select(i => new int[n + 1]).ToArray();
+        for (int i = 1; i < m + 1; i++)
         {
             for (int j = 1; j < n + 1; j++)
             {
@@ -85,7 +77,7 @@ public class Solution
                 }
             }
         }
-        return Convert.ToInt32(dp(0,0,k-1) % MOD);
+        return Convert.ToInt32(dp(0,0,k-1));
     }
 }
 // @lc code=end
