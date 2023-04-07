db49d7eda4fdafbeb7d293caf4c9182c9e81cff0
huikinglam02gmail <huikinglam02@gmail.com>
2023-04-07 14:59:51 -0400

    1045_040723

---
+++ 1045.customers-who-bought-all-products.sql
@@ -0,0 +1,14 @@
+--
+-- @lc app=leetcode id=1045 lang=mysql
+--
+-- [1045] Customers Who Bought All Products
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT(product_key)) = (SELECT COUNT(*) FROM Product)
+-- @lc code=end
+
