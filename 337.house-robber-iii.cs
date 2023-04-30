51fbc5058472a623c5e2593ec33162df8a40b87e
huikinglam02gmail <huikinglam02@gmail.com>
2023-04-29 22:14:14 -0400

    337_042923

--- 337.house-robber-iii.cs
+++ 337.house-robber-iii.cs
@@ -18,12 +18,45 @@
  *     }
  * }
  */
+using System.Collections.Generic;
+using System;
 public class Solution 
 {
-    
+    private Dictionary<Tuple<int, bool>, int> memo;
+
+    public int dfs(TreeNode node, int nodeID, bool robThisNode)
+    {
+        Tuple<int, bool> key = new Tuple<int, bool>(nodeID, robThisNode);
+        if (memo.ContainsKey(key))
+        {
+            return memo[key];
+        }
+        else
+        {
+            int result = 0;
+            if (node != null)
+            {
+                if (robThisNode)
+                {
+                    result += node.val;
+                    result += dfs(node.left, 2 * nodeID + 1, false);
+                    result += dfs(node.right, 2 * nodeID + 2, false);
+                }
+                else
+                {
+                    result += Math.Max(dfs(node.left, 2 * nodeID + 1, false), dfs(node.left, 2 * nodeID + 1, true));
+                     result += Math.Max(dfs(node.right, 2 * nodeID + 2, false), dfs(node.right, 2 * nodeID + 2, true));                   
+                }
+            }
+            memo.Add(key, result);
+            return result;
+        }
+    }
+
     public int Rob(TreeNode root) 
     {
-        
+        memo = new Dictionary<Tuple<int, bool>, int>();
+        return Math.Max(dfs(root, 0, true), dfs(root, 0, false));
     }
 }
 // @lc code=end
