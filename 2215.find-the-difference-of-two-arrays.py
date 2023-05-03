5045381bbc19ac5ed7378d3321f1d3ee15d362d7
huikinglam02gmail <huikinglam02@gmail.com>
2023-05-02 21:37:11 -0400

    1321_2215_050223

---
+++ 2215.find-the-difference-of-two-arrays.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2215 lang=python3
+#
+# [2215] Find the Difference of Two Arrays
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        return [list((set(nums1)^set(nums2))&set(nums1)),list((set(nums2)^set(nums1))&set(nums2))]
+         
+# @lc code=end
+
