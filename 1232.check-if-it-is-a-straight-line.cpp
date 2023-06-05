ea368e30e650c9300bb557534c9728c1146b774b
huikinglam02gmail <huikinglam02@gmail.com>
2023-06-04 22:46:43 -0400

    1232_1735_060423

---
+++ 1232.check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1232 lang=cpp
+ *
+ * [1232] Check If It Is a Straight Line
+ */
+
+// @lc code=start
+#include <vector>;
+using std::vector;
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        
+    }
+};
+// @lc code=end
+
