a9743b35e17ad565fd8d97d1698bb3237c29d7ec
huikinglam02gmail <huikinglam02@gmail.com>
2023-09-16 22:04:18 -0400

    1898_091623

--- 1898.maximum-number-of-removable-characters.cs
+++ 1898.maximum-number-of-removable-characters.cs
@@ -10,32 +10,36 @@ using System.Collections.Generic;
 
 public class Solution {
     private Dictionary<int, int> reverseRemovable;
-    public bool IsSubsequence(string s, string t, int mid) {
+    private string s;
+    private string p;
+    public bool IsSubsequence(int mid) {
         int p1 = 0, p2 = 0;
-        while (p1 < s.Length && p2 < t.Length) {
-            if (s[p1] == t[p2] && (!reverseRemovable.ContainsKey(p2) || reverseRemovable[p2] >= mid)) {
+        while (p1 < p.Length && p2 < s.Length) {
+            if (p[p1] == s[p2] && (!reverseRemovable.ContainsKey(p2) || reverseRemovable[p2] >= mid)) {
                 p1++;
             }
             p2++;
         }
-        return p1 == s.Length;
+        return p1 == p.Length;
     }
 
     public int MaximumRemovals(string s, string p, int[] removable) {
         int l = 0, r = removable.Length;
+        this.s = s;
+        this.p = p;
         reverseRemovable = new Dictionary<int, int>();
         for (int i = 0; i < removable.Length; i++) {
             reverseRemovable[removable[i]] = i;
         }
         while (l < r) {
             int mid = l + (r - l) / 2;
-            if (IsSubsequence(p, s, mid)) {
+            if (IsSubsequence(mid)) {
                 l = mid + 1;
             } else {
                 r = mid;
             }
         }
-        if (l == removable.Length && IsSubsequence(p, s, l)) {
+        if (l == removable.Length && IsSubsequence(l)) {
             return l;
         } else {
             return l - 1;
--- 1898.maximum-number-of-removable-characters.py
+++ 1898.maximum-number-of-removable-characters.py
@@ -13,27 +13,29 @@ class Solution:
     Then binary search
     Use isSubsequence
     '''
-    def isSubsequence(self, s: str, t: str, mid) -> bool:
+    def isSubsequence(self, mid) -> bool:
         p1, p2 = 0, 0
-        while p1 < len(s) and p2 < len(t):
-            if s[p1] == t[p2] and (p2 not in self.reverseRemovable or self.reverseRemovable[p2] >= mid):
+        while p1 < len(self.p) and p2 < len(self.s):
+            if self.p[p1] == self.s[p2] and (p2 not in self.reverseRemovable or self.reverseRemovable[p2] >= mid):
                 p1 += 1
             p2 += 1
-        return p1 == len(s)
+        return p1 == len(self.p)
     
 
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
         l, r = 0, len(removable)
+        self.s = s
+        self.p = p
         self.reverseRemovable = {}
         for i in range(len(removable)):
             self.reverseRemovable[removable[i]] = i
         while l < r:
             mid = l + (r - l) // 2
-            if self.isSubsequence(p, s, mid):
+            if self.isSubsequence(mid):
                 l = mid + 1
             else:
                 r = mid
-        if l == len(removable) and self.isSubsequence(p, s, l):
+        if l == len(removable) and self.isSubsequence(l):
             return l
         else:
             return l - 1
