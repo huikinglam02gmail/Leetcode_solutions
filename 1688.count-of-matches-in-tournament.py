e0ddbb283cc3b4e493857e6151b9b04df47fd04d
huikinglam02gmail <huikinglam02@gmail.com>
2023-12-04 22:11:15 -0500

    1688_120423

---
+++ 1688.count-of-matches-in-tournament.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1688 lang=python3
+#
+# [1688] Count of Matches in Tournament
+#
+
+# @lc code=start
+class Solution:
+    '''
+    In each game one person is kicked out.
+    '''
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
+# @lc code=end
+
