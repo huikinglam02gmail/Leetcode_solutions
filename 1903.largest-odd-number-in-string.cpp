2e4d109bd8d6f1af52fbde0fc3ecd55a4b470e59
huikinglam02gmail <huikinglam02@gmail.com>
2023-12-06 21:43:33 -0500

    1903_120623

---
+++ 1903.largest-odd-number-in-string.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1903 lang=cpp
+ *
+ * [1903] Largest Odd Number in String
+ */
+
+// @lc code=start
+
+class Solution {
+public:
+    std::string largestOddNumber(std::string num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
+
+// @lc code=end
+
