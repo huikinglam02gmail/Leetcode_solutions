99bf31429503d41b00c93841529306df72882772
huikinglam02gmail <huikinglam02@gmail.com>
2024-01-29 22:21:21 -0500

    2678_012924

--- 2678.number-of-senior-citizens.py
+++ 2678.number-of-senior-citizens.py
@@ -1,1 +1,17 @@
-0
+#
+# @lc app=leetcode id=2678 lang=python3
+#
+# [2678] Number of Senior Citizens
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        result = 0
+        for detail in details:
+            if int(detail[11:13]) > 60: result += 1
+        return result
+# @lc code=end
