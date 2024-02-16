f2e7cae262b5fcc2a4d86862b46f9714e7c39475
huikinglam02gmail <huikinglam02@gmail.com>
2024-02-15 23:56:49 -0500

    1481_021524

--- 1481.least-number-of-unique-integers-after-k-removals.cpp
+++ 1481.least-number-of-unique-integers-after-k-removals.cpp
@@ -5,11 +5,37 @@
  */
 
 // @lc code=start
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    int findLeastNumOfUniqueInts(std::vector<int>& arr, int k) {
+        std::unordered_map<int, int> occur;
+        for (int num : arr) {
+            occur[num]++;
+        }
         
+        std::vector<std::pair<int, int>> kvp(occur.begin(), occur.end());
+        std::sort(kvp.begin(), kvp.end(), [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        });
+        
+        int n = kvp.size();
+        int i = 0;
+        while (k > 0) {
+            if (kvp[i].second <= k) {
+                k -= kvp[i].second;
+                i++;
+            } else {
+                k = 0;
+            }
+        }
+        
+        return n - i;
     }
 };
+
 // @lc code=end
 
