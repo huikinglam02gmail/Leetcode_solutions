d211338ad23cdc6358b247605794071457235be4
huikinglam02gmail <huikinglam02@gmail.com>
2024-02-25 21:45:26 -0500

    100_022524

---
+++ 2913.subarrays-distinct-element-sum-of-squares-i.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2913 lang=python3
+#
+# [2913] Subarrays Distinct Element Sum of Squares I
+#
+
+# @lc code=start
+class Solution:
+    def sumCounts(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
