e12fa3931be8df0dc252b5081299d40738e94e16
huikinglam02gmail <huikinglam02@gmail.com>
2024-05-30 00:49:08 -0400

    1442_053024

--- 1442.count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442.count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -7,23 +7,24 @@
 # @lc code=start
 from typing import List
 class Solution:
-    # The problem is asking for contiguous nonempty subarrays which have equal XOR
-    # But we XOR them together, we will always arrive at 0
-    # So the problem is to find all subarrays with 0 XOR
-    # Each with contribute len(0 XOR subarray) - 1 to the result
-
+    '''
+    The problem is asking for contiguous nonempty subarrays which have equal XOR
+    But we XOR them together, we will always arrive at 0
+    So the problem is to find all subarrays with 0 XOR
+    Each with contribute len(0 XOR subarray) - 1 to the result    
+    '''
     def countTriplets(self, arr: List[int]) -> int:
         prefix = [0]
         Seen = {0: [0]}
         result = 0
         for i, num in enumerate(arr):
-            prefix.append(prefix[-1]^num)
+            prefix.append(prefix[-1] ^ num)
             if prefix[-1] in Seen:
                 for j in Seen[prefix[-1]]:
                     result += i - j
             else:
                 Seen[prefix[-1]] = []
-            Seen[prefix[-1]].append(i+1)
+            Seen[prefix[-1]].append(i + 1)
         return result
 # @lc code=end
 
