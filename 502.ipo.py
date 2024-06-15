e6c92aa680cad6cad40ee1871e81528dcc13abe3
huikinglam02gmail <huikinglam02@gmail.com>
2024-06-14 21:48:13 -0400

    502_061424

--- 502.ipo.py
+++ 502.ipo.py
@@ -6,6 +6,7 @@
 
 # @lc code=start
 import heapq
+from typing import List
 
 
 class Solution:
@@ -17,8 +18,7 @@ class Solution:
     '''
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
         couples = []
-        for i in range(len(capital)):
-            couples.append((capital[i], profits[i]))
+        for i in range(len(capital)): couples.append((capital[i], profits[i]))
         couples.sort()
         heap = []
         i = 0
@@ -26,8 +26,7 @@ class Solution:
             while i < len(couples) and couples[i][0] <= w:
                 heapq.heappush(heap, - couples[i][1])
                 i += 1
-            if not heap:
-                return w
+            if not heap: return w
             w -= heapq.heappop(heap)
             k -= 1
         return w
