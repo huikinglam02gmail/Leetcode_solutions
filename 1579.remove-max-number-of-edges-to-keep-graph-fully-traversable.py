02e76c242d073677ccd27753044945678d0002d0
huikinglam02gmail <huikinglam02@gmail.com>
2024-06-29 21:19:37 -0400

    1579_062924

--- 1579.remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579.remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -25,24 +25,23 @@ class UnionFindSet:
             self.count -= 1
 
 class Solution:
-    # We can collect the edges usable by Alice and Bob respectively
-    # Then just build the graph from scratch.
-    # For both cases, use the shared edges first
-    # We arrive at the minimal necessary skeleton when the size of disjoint set has arrived at n
-    # The intersection between unused edges by Alice and Bob are those that can be disposed
-    
+    '''
+    We can collect the edges usable by Alice and Bob respectively
+    Then just build the graph from scratch.
+    For both cases, use the shared edges first
+    We arrive at the minimal necessary skeleton when the size of disjoint set has arrived at n
+    The intersection between unused edges by Alice and Bob are those that can be disposed    
+    ''' 
     def buildGraph(self, canToss, edges):
         edgeCount = len(edges)
         ind = 0
-        while ind < edgeCount and self.DSU.count > 1:
-            i, u, v = edges[ind]
-            if self.DSU.find(u) == self.DSU.find(v):
-                canToss.add(i)
-            else:
-                self.DSU.union(u, v)
-            ind += 1
         while ind < edgeCount:
-            canToss.add(edges[ind][0])
+            if self.DSU.count > 1:
+                i, u, v = edges[ind]
+                if self.DSU.find(u) == self.DSU.find(v): canToss.add(i)
+                else: self.DSU.union(u, v)
+            else:
+                canToss.add(edges[ind][0])
             ind += 1
         return canToss
     
@@ -73,8 +72,7 @@ class Solution:
                 self.DSU.parents = defaultParents
                 self.DSU.count = defaultCount
             canToss = self.buildGraph(canToss, specific)
-            if self.DSU.count > 1:
-                return -1
+            if self.DSU.count > 1: return -1
         return len(canToss)
 # @lc code=end
 
