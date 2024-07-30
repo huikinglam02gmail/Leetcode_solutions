29b7f877dfdbb478f29d08823d515efda1bae3de
huikinglam02gmail <huikinglam02@gmail.com>
2024-07-29 22:24:40 -0400

    1653_072924

--- 1653.minimum-deletions-to-make-string-balanced.py
+++ 1653.minimum-deletions-to-make-string-balanced.py
@@ -9,23 +9,25 @@ import bisect
 
 
 class Solution:
-    # Record the appearance of a and b at different indices
-    # Then for each index, ask how many a I need to delete right of it + how many b I need to delete left of it
-    # Take the minimum
+    '''
+    Record the appearance of a and b at different indices
+    Then for each index, ask how many a I need to delete right of it + how many b I need to delete left of it
+    Take the minimum    
+    '''
     def minimumDeletionsBinarySearch(self, s: str) -> int:
         hashTable, n = {}, len(s)
         hashTable['a'] = []
         hashTable['b'] = []
-        for i, c in enumerate(s):
-            hashTable[c].append(i)
+        for i, c in enumerate(s): hashTable[c].append(i)
         
         result = n
-        for i in range(n):
-            result = min(result, bisect.bisect_right(hashTable['b'], i - 1) + len(hashTable['a']) - bisect.bisect_left(hashTable['a'], i + 1))
+        for i in range(n): result = min(result, bisect.bisect_right(hashTable['b'], i - 1) + len(hashTable['a']) - bisect.bisect_left(hashTable['a'], i + 1))
         return result
 
-    # There is an O(n) way to do the problem
-    # We practically need to find number of as and bs before and after each index
+    '''
+    There is an O(n) way to do the problem
+    We practically need to find number of as and bs before and after each index
+    '''
     def minimumDeletions(self, s: str) -> int:
         rightA, n = s.count('a'), len(s)
         leftB, result = 0, rightA
