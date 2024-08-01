96238b8ee1d328cc525b078bea92227e57930359
huikinglam02gmail <huikinglam02@gmail.com>
2024-07-31 20:51:39 -0400

    2678_073124

--- 2678.number-of-senior-citizens.py
+++ 2678.number-of-senior-citizens.py
@@ -11,7 +11,7 @@ from typing import List
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
         result = 0
-        for detail in details:
+        for detail in details: 
             if int(detail[11:13]) > 60: result += 1
         return result
 # @lc code=end
