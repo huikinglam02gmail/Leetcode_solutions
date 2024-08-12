7cd2ff771057e8a7f38ebdbc63db560431c66ff5
huikinglam02gmail <huikinglam02@gmail.com>
2024-08-11 21:41:47 -0400

    703_081124

--- 703.kth-largest-element-in-a-stream.py
+++ 703.kth-largest-element-in-a-stream.py
@@ -18,19 +18,12 @@ class KthLargest:
         self.size = k
         for num in nums:
             heapq.heappush(self.heap, num)
-            while len(self.heap) > k:
-                heapq.heappop(self.heap)
+            while len(self.heap) > k: heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
         heapq.heappush(self.heap, val)
-        if len(self.heap) > self.size:
-            heapq.heappop(self.heap)
+        if len(self.heap) > self.size: heapq.heappop(self.heap)
         return self.heap[0]
-
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
