eb119af60e026a23a05b7010d245dcb6d30a84fb
huikinglam02gmail <huikinglam02@gmail.com>
2024-10-02 21:40:31 -0400

    1590_100224

--- 1590.make-sum-divisible-by-p.py
+++ 1590.make-sum-divisible-by-p.py
@@ -5,18 +5,21 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
-    # let's calculate the prefix sum
-    # we are looking for subarrays in which (Total - (PS[j] - PS[i])) % p == 0
-    # PS[j] % p == (Total + PS[i]) % p = Total % p + PS[i] % p
-    # Therefore we look for PS[i] % p = PS[j] % p - Total % p (if positive) or PS[j] % p - Total % p + p
+    '''
+    let's calculate the prefix sum
+    we are looking for subarrays in which (Total - (PS[j] - PS[i])) % p == 0
+    PS[j] % p == (Total + PS[i]) % p = Total % p + PS[i] % p
+    Therefore we look for PS[i] % p = PS[j] % p - Total % p (if positive) or PS[j] % p - Total % p + p
+    '''
     def minSubarray(self, nums: List[int], p: int) -> int:
         total = sum(nums)
         result = len(nums)
-        if total % p == 0:
-            return 0
-        elif total < p:
-            return -1
+        if total % p == 0: return 0
+        elif total < p: return -1
         
         prefix = [0]
         hashTable = {}
@@ -24,13 +27,10 @@ class Solution:
         for i, num in enumerate(nums):
             prefix.append(prefix[-1] + num)
             keyToLookFor = (p + prefix[-1] % p - total % p) % p 
-            if keyToLookFor in hashTable:
-                result = min(result, i - hashTable[keyToLookFor])
+            if keyToLookFor in hashTable: result = min(result, i - hashTable[keyToLookFor])
             hashTable[prefix[-1] % p] = i
-        if result < len(nums):
-            return result
-        else:
-            return -1
+        if result < len(nums): return result
+        else: return -1
 
 
 # @lc code=end
