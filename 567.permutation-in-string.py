3e29bc303b3859917640c5a450597d446b1dd40f
huikinglam02gmail <huikinglam02@gmail.com>
2024-10-04 21:12:56 -0400

    567_100424

--- 567.permutation-in-string.py
+++ 567.permutation-in-string.py
@@ -6,18 +6,15 @@
 
 # @lc code=start
 class Solution:
-    # sliding window
+    '''
+    sliding window    
+    '''
     def checkAppearances(self, count1, count2):
         count = 0
         for i in range(26):
-            if count1[i] > count2[i]:
-                return -1
-            elif count1[i] == count2[i]:
-                count += 1
-        if count == 26:
-            return 0
-        else:
-            return 1
+            if count1[i] > count2[i]: return -1
+            elif count1[i] == count2[i]: count += 1
+        return abs(count - 26) > 0
 
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2) < len(s1):
@@ -25,8 +22,7 @@ class Solution:
         else:
             seen1 = [0]*26
             seen2 = [0]*26
-            for c in s1:
-                seen1[ord(c) - ord('a')] += 1
+            for c in s1: seen1[ord(c) - ord('a')] += 1
             left = 0
             
             for right in range(len(s2)):
@@ -34,8 +30,7 @@ class Solution:
                 while self.checkAppearances(seen1, seen2) == 1:
                     seen2[ord(s2[left]) - ord('a')] -= 1
                     left += 1
-                if self.checkAppearances(seen1, seen2) == 0:
-                    return True
+                if self.checkAppearances(seen1, seen2) == 0: return True
             return False
 # @lc code=end
 
