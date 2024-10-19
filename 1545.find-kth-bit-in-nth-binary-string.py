56ac5e5eea8999e30fd4e603284e9fb7d0ecda82
huikinglam02gmail <huikinglam02@gmail.com>
2024-10-18 22:25:40 -0400

    1545_101824

--- 1545.find-kth-bit-in-nth-binary-string.py
+++ 1545.find-kth-bit-in-nth-binary-string.py
@@ -6,16 +6,14 @@
 
 # @lc code=start
 class Solution:
-    # If we look for the mid element, it is always 1 except for n = 1
-    # Otherwise just find if it is left or right of the mid element
+    '''
+    If we look for the mid element, it is always 1 except for n = 1
+    Otherwise just find if it is left or right of the mid element    
+    '''
     def findKthBit(self, n: int, k: int) -> str:
-        if n == 1:
-            return "0"
-        if k == pow(2, n - 1):
-            return "1"
-        elif k > pow(2, n - 1):
-            return str(1 - int(self.findKthBit(n - 1, pow(2, n) - k)))
-        else:
-            return self.findKthBit(n - 1, k)
+        if n == 1: return "0"
+        if k == pow(2, n - 1): return "1"
+        elif k > pow(2, n - 1): return str(1 - int(self.findKthBit(n - 1, pow(2, n) - k)))
+        else: return self.findKthBit(n - 1, k)
 # @lc code=end
 
