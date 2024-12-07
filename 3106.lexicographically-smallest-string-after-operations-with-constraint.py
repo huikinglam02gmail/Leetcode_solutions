29455984bcc87755d39dd702d79d06a162228cb8
huikinglam02gmail <huikinglam02@gmail.com>
2024-12-06 22:41:23 -0500

    3106_120624

--- 3106.lexicographically-smallest-string-after-operations-with-constraint.py
+++ 3106.lexicographically-smallest-string-after-operations-with-constraint.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def getSmallestString(self, s: str, k: int) -> str:
-        
+        newString = ""
+        for c in s:
+            if k > 0:
+                steps = min(ord(c) - ord('a'), ord('a') + 26 - ord(c))
+                if steps <= k:
+                    newString += 'a'
+                    k -= steps
+                else:
+                    newString += chr(ord(c) - k)
+                    k = 0
+            else: newString += c
+        return newString
+                    
 # @lc code=end
 
