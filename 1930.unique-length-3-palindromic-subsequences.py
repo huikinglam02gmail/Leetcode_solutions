d947c8d71b6ed67c13a575ba6994ea2277db3ca3
huikinglam02gmail <huikinglam02@gmail.com>
2025-01-03 20:16:51 -0500

    1930_010325

--- 1930.unique-length-3-palindromic-subsequences.py
+++ 1930.unique-length-3-palindromic-subsequences.py
@@ -14,14 +14,12 @@ class Solution:
     '''
     def countPalindromicSubsequence(self, s: str) -> int:
         occur = [[] for i in range(26)]
-        for i, c in enumerate(s):
-            occur[ord(c) - ord('a')].append(i)
+        for i, c in enumerate(s): occur[ord(c) - ord('a')].append(i)
         result = 0
         for i in range(26):
             if len(occur[i]) > 1:
                 for j in range(26):
-                    if bisect.bisect_left(occur[j], occur[i][-1]) != bisect.bisect_right(occur[j], occur[i][0]):
-                        result += 1
+                    if bisect.bisect_left(occur[j], occur[i][-1]) != bisect.bisect_right(occur[j], occur[i][0]): result += 1
         return result
 # @lc code=end
 
