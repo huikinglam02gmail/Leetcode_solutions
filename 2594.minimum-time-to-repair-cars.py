3425e5b873f3474cba23b40bde1c25c19ff9ca2a
huikinglam02gmail <huikinglam02@gmail.com>
2025-01-15 23:34:52 -0500

    2594_011525

--- 2594.minimum-time-to-repair-cars.py
+++ 2594.minimum-time-to-repair-cars.py
@@ -5,11 +5,24 @@
 #
 
 # @lc code=start
+import math
 from typing import List
 
 
 class Solution:
-    def repairCars(self, ranks: List[int], cars: int) -> int:
-        
-# @lc code=end
+    '''
+    Binary search for the ans
+    '''
+    def numberOfCarsRepaired(self, ranks, t):
+        result = 0
+        for rank in ranks: result += int(math.sqrt(t // rank)) 
+        return result
 
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        l, r = 0, min(ranks) * cars * cars + 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if self.numberOfCarsRepaired(ranks, mid) < cars: l = mid + 1
+            else: r = mid
+        return l
+    # @lc code=end
