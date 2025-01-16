e07d3b4eefea10871d8a4e2ee834809fd0917569
huikinglam02gmail <huikinglam02@gmail.com>
2025-01-15 20:59:04 -0500

    2594_011525

---
+++ 2594.minimum-time-to-repair-cars.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=2594 lang=python3
+#
+# [2594] Minimum Time to Repair Cars
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        
+# @lc code=end
+
