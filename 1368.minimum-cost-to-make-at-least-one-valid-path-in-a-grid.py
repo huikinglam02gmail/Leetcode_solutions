9c6a06f8e054ee0c55dc7f9abf348a945dd1bb64
huikinglam02gmail <huikinglam02@gmail.com>
2025-01-17 23:24:05 -0500

    1368_011725

--- 1368.minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1368.minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -10,24 +10,29 @@ from typing import List
 
 
 class Solution:
-    # Typical Dijkstra graph search problem
-    # We can modify the grid as we go
+    '''
+    Typical Dijkstra graph search problem
+    We can modify the grid as we go    
+    '''
     def minCost(self, grid: List[List[int]]) -> int:
         heap, m, n = [], len(grid), len(grid[0])
         heapq.heappush(heap, [0, 0, 0])
+        minSoFar = [[float("inf") for j in range(n)] for i in range(m)]
         while heap:
             w, x, y = heapq.heappop(heap)
-            if x == m-1 and y == n-1:
-                return w
-            neigs = [[x,y+1],[x,y-1],[x+1,y],[x-1,y]]
+            if x == m - 1 and y == n - 1: return w
+            neigs = [[x, y + 1],[x, y - 1],[x + 1, y],[x - 1, y]]
             arrow = grid[x][y] - 1
             for i, neig in enumerate(neigs):
                 xn, yn = neig
                 if m > xn >= 0 <= yn < n and 1 <= grid[xn][yn] <= 4:
-                    if i == arrow:
+                    if i == arrow and w < minSoFar[xn][yn]: 
                         heapq.heappush(heap, [w, xn, yn])
-                    else:
-                        heapq.heappush(heap, [w+1, xn, yn])
+                        minSoFar[xn][yn] = w
+                    elif w + 1 < minSoFar[xn][yn]: 
+                        heapq.heappush(heap, [w + 1, xn, yn])
+                        minSoFar[xn][yn] = w + 1
             grid[x][y] = 5
+        return -1
 # @lc code=end
 
