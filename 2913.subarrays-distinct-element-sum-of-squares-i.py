bcd7ba9c46b0630804f9ac6e2a3cf1b3476f4590
huikinglam02gmail <huikinglam02@gmail.com>
2025-03-17 19:45:54 -0400

    2913_031725

--- 2913.subarrays-distinct-element-sum-of-squares-i.py
+++ 2913.subarrays-distinct-element-sum-of-squares-i.py
@@ -5,8 +5,17 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def sumCounts(self, nums: List[int]) -> int:
-        
+        result = 0
+        for i in range(len(nums)):
+            hashTable = {}
+            for j in range(i, len(nums)):
+                hashTable[nums[j]] = hashTable.get(nums[j], 0) + 1
+                result += len(hashTable) * len(hashTable)
+        return result
 # @lc code=end
 
