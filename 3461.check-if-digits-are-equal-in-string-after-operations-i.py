8ee5c001793362d0fe1f76799864603529ce262b
huikinglam02gmail <huikinglam02@gmail.com>
2025-03-17 20:21:08 -0400

    3461_031725

--- 3461.check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3461.check-if-digits-are-equal-in-string-after-operations-i.py
@@ -7,6 +7,10 @@
 # @lc code=start
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
-        
+        if len(s) == 2: return s[0] == s[1]
+        newS = ""
+        for i in range(len(s) - 1): newS += str((int(s[i]) + int(s[i + 1])) % 10)
+        return self.hasSameDigits(newS)
+
 # @lc code=end
 
