e3afa9c1c67a769eb276e9ed5ef980da670d9e07
huikinglam02gmail <huikinglam02@gmail.com>
2025-03-21 01:05:58 -0400

    3318_032125

--- 3318.find-x-sum-of-all-k-long-subarrays-i.py
+++ 3318.find-x-sum-of-all-k-long-subarrays-i.py
@@ -5,12 +5,64 @@
 #
 
 # @lc code=start
-import heapq
 from typing import List
 
 
 class Solution:
+    '''
+    We keep two sorted lists, one keeping the top x most frequent elements and the second keeping the rest
+    Also keep track of occurrence of all the elements in a hash table
+    '''
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        
+        occur = {}
+        self.top = SortedList()
+        self.bottom = SortedList()
+        result = []
+        self.S = 0
+        for i in range(len(nums)):
+            if i >= k:
+                fromTop = False
+                if [occur[nums[i - k]], nums[i - k]] in self.top: 
+                    self.top.remove([occur[nums[i - k]], nums[i - k]])
+                    self.S -= occur[nums[i - k]] * nums[i - k]
+                    fromTop = True
+                else: 
+                    self.bottom.remove([occur[nums[i - k]], nums[i - k]])
+                occur[nums[i - k]] -= 1
+                if occur[nums[i - k]] == 0: occur.pop(nums[i - k])
+                elif not fromTop: 
+                    self.top.add([occur[nums[i - k]], nums[i - k]])
+                    self.S += occur[nums[i - k]] * nums[i - k]
+                else:
+                    self.bottom.add([occur[nums[i - k]], nums[i - k]])
+            self.balance(x)
+            fromTop = False                                   
+            if nums[i] not in occur: occur[nums[i]] = 0
+            elif [occur[nums[i]], nums[i]] in self.top:
+                self.top.remove([occur[nums[i]], nums[i]])
+                self.S -= occur[nums[i]] * nums[i]
+                fromTop = True
+            else:
+                self.bottom.remove([occur[nums[i]], nums[i]])
+            occur[nums[i]] += 1
+            if not fromTop:
+                self.top.add([occur[nums[i]], nums[i]])
+                self.S += occur[nums[i]] * nums[i]
+            else:
+                self.bottom.add([occur[nums[i]], nums[i]])
+            self.balance(x)
+            if i >= k - 1: result.append(self.S)
+        return result
+    
+    def balance(self, x):
+        while len(self.top) > x:
+            freq, num = self.top.pop(0)
+            self.S -= freq * num
+            self.bottom.add([freq, num])
+        while len(self.top) < x and len(self.bottom) > 0:
+            freq, num = self.bottom.pop(-1)
+            self.S += freq * num
+            self.top.add([freq, num])
+
 # @lc code=end
 
