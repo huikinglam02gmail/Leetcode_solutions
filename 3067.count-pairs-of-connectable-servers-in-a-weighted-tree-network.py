750d97d2e3940fbade28145d05bfd93ae080adfb
huikinglam02gmail <huikinglam02@gmail.com>
2025-03-23 23:39:00 -0400

    3067_032325

--- 3067.count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
+++ 3067.count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
@@ -10,23 +10,41 @@ from typing import List
 
 class Solution:
     def countPairsOfConnectableServers(self, edges: List[List[int]], signalSpeed: int) -> List[int]:
-        self.graph = [set() for i in range(len(edges) + 1)]
+        self.graph = [{} for i in range(len(edges) + 1)]
+        self.signalSpeed = signalSpeed
         for a, b, w in edges:
-            if b not in self.graph[a]: self.graph[a] = {}
             self.graph[a][b] = w
-            if a not in self.graph[b]: self.graph[b] = {}
             self.graph[b][a] = w
         
-
+        result = [0] * (len(edges) + 1)
+        for i in range(len(edges) + 1):
+            pathSumDicts = []
+            for nxt in self.graph[i]:
+                pathSumDicts.append({})
+                currentKey = self.graph[i][nxt] % signalSpeed
+                pathSumDicts[-1][currentKey] = 1
+                for k, v in self.dfs(nxt, i).items():
+                    childrenKey = (self.graph[i][nxt] + k) % signalSpeed
+                    pathSumDicts[-1][childrenKey] = pathSumDicts[-1].get(childrenKey, 0) + v
+            connectables = []
+            for pathSumDict in pathSumDicts:
+                connectables.append(0)
+                connectables[-1] += pathSumDict.get(0, 0)
+            for j in range(len(connectables) - 1):
+                for k in range(j + 1, len(connectables)): 
+                    result[i] += connectables[j] * connectables[k]
+        return result
+             
     def dfs(self, node, prev):
-        pathSums = []
+        pathSumDict = {}
         for nxt in self.graph[node]:
             if nxt != prev:
-                childrenPathSums = self.dfs(nxt, node)
-                pathSums.append([])
-                pathSums[-1].append([self.graph[node][nxt]])
-                for childrenPathSum in childrenPathSums: 
-                    pathSums[-1].append(self.graph[node][nxt] + childrenPathSum)
-        return pathSums        
+                currentKey = self.graph[node][nxt] % self.signalSpeed
+                pathSumDict[currentKey] = pathSumDict.get(currentKey, 0) + 1
+                childrenDict = self.dfs(nxt, node)
+                for child in childrenDict:
+                    childrenKey = (self.graph[node][nxt] + child) % self.signalSpeed
+                    pathSumDict[childrenKey] = pathSumDict.get(childrenKey, 0) + childrenDict[child]
+        return pathSumDict    
 # @lc code=end
 
