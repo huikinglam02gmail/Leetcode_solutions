55dd74fbfdf1aabd20b5d0597fda35942f2cfe53
huikinglam02gmail <huikinglam02@gmail.com>
2025-03-30 20:42:34 -0400

    2551_033025

--- 2551.put-marbles-in-bags.py
+++ 2551.put-marbles-in-bags.py
@@ -5,17 +5,18 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     '''
     Find k max / min pairs of weights[0] + weights[-1] + (weights[i] + weight[i+1]), i from 0 to n - 2    
     '''
 
     def putMarbles(self, weights: List[int], k: int) -> int:
-        if k == 1:
-            return 0
+        if k == 1: return 0
         data = []
-        for i in range(len(weights) - 1):
-            data.append(weights[i] + weights[i+1])
+        for i in range(len(weights) - 1): data.append(weights[i] + weights[i+1])
         data.sort()
         return sum(data[(-k + 1):]) - sum(data[:(k - 1)])
         
