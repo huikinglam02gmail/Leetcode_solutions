4950bffb4f1e30b1c593afacddf2ac6d688b4e16
huikinglam02gmail <huikinglam02@gmail.com>
2025-12-13 01:16:33 -0500

    97_121325

--- 97.interleaving-string.py
+++ 97.interleaving-string.py
@@ -12,17 +12,17 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
-        dp = [[False for i in range(len(s2)+1)] for j in range(len(s1)+1)]
-        for i in range(len(s1)+1):
-            for j in range(len(s2)+1):
+        dp = [[False for i in range(len(s2) + 1)] for j in range(len(s1) + 1)]
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
                 if i == 0 and j == 0:
                     dp[i][j] = True
                 elif i == 0:
-                    dp[i][j] = s2[j-1] == s3[j-1] and dp[i][j-1]
+                    dp[i][j] = s2[j - 1] == s3[j - 1] and dp[i][j - 1]
                 elif j == 0:
-                    dp[i][j] = s1[i-1] == s3[i-1] and dp[i-1][j]
+                    dp[i][j] = s1[i - 1] == s3[i - 1] and dp[i - 1][j]
                 else:
-                    dp[i][j] = (dp[i][j-1] and s2[j-1] == s3[i+j-1]) or (dp[i-1][j] and s1[i-1] == s3[i+j-1])
+                    dp[i][j] = (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]) or (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1])
         return dp[len(s1)][len(s2)]
 # @lc code=end
 
