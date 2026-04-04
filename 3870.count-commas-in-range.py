b774a5df55273ed0e418f9759d744a615e9dc77b
huikinglam02gmail <huikinglam02@gmail.com>
2026-04-04 00:03:47 -0400

    3870_3875_040426

---
+++ 3870.count-commas-in-range.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=3870 lang=python3
+#
+# [3870] Count Commas in Range
+#
+
+# @lc code=start
+class Solution:
+    def countCommas(self, n: int) -> int:
+        return max(0, n + 1 - 1000)
+# @lc code=end
+
---
+++ 3875.construct-uniform-parity-array-i.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=3875 lang=python3
+#
+# [3875] Construct Uniform Parity Array I
+#
+
+# @lc code=start
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+
